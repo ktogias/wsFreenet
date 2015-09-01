deaac5cba5002806946a27536fee2b293fb2fac9
Konstantinos Togias <info@ktogias.gr>
2015-09-01 11:27:47 +0300

    Plugin setup

---
+++ src/plugins/WSFreenet/WSFreenet.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package plugins.WSFreenet;
+
+import freenet.pluginmanager.FredPlugin;
+import freenet.pluginmanager.FredPluginThreadless;
+import freenet.pluginmanager.PluginRespirator;
+
+/**
+ *
+ * @author ktogias
+ */
+public class WSFreenet implements FredPlugin, FredPluginThreadless{
+
+    @Override
+    public void terminate() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void runPlugin(PluginRespirator pr) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
